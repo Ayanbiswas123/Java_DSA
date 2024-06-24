ac2bd98191f6154405b6dfb5dca812cdadd5a3a1
Ayan <ayankgp1@gmail.com>
2024-06-24 13:04:58 +0530

    Upgrade print array

--- 01. Array/print_array.java
+++ 01. Array/print_array.java
@@ -2,9 +2,5 @@ public class print_array {
 
     public static void main(String[] args) {
         System.out.println("helllo");
-        System.out.println("helllo");
-        System.out.println("helllo");
-        System.out.println("helllo");
-        System.out.println("helllo");
     }
 }
