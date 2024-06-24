4d74c6c5b7e20665e1e071b38748f9956c3ef58f
Ayan <ayankgp1@gmail.com>
2024-06-24 12:56:47 +0530

    print array updated

--- 01. Array/print_array.java
+++ 01. Array/print_array.java
@@ -2,5 +2,7 @@ public class print_array {
 
     public static void main(String[] args) {
         System.out.println("helllo");
+        System.out.println("helllo");
+        System.out.println("helllo");
     }
 }
