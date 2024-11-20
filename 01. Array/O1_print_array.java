a10521cef3f78468dd34d0bfe07e3b605c4fb5f5
Ayan <ayankgp1@gmail.com>
2024-11-20 17:36:04 +0530

    add test in array

--- 01. Array/O1_print_array.java
+++ 01. Array/O1_print_array.java
@@ -2,6 +2,6 @@ public class O1_print_array {
 
     public static void main(String[] args) {
         System.out.println("helllo");
-        System.out.println("helllo");
+        System.out.println("helllo world");
     }
 }
