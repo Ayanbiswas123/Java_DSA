7776d4e83aec78c1d223f9ed03e4fb20b0b33738
Ayan <ayankgp1@gmail.com>
2024-11-21 17:36:40 +0530

    Add one line in array

--- 01. Array/O1_print_array.java
+++ 01. Array/O1_print_array.java
@@ -3,5 +3,6 @@ public class O1_print_array {
     public static void main(String[] args) {
         System.out.println("helllo");
         System.out.println("helllo world");
+        System.out.println("helllo world from main");
     }
 }
